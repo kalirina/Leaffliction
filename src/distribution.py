f6c707b04b84ce2d0319a61dc532be9c8719bb5a
irkalini <irakalina2004@gmail.com>
2026-06-24 17:01:54 +0200

    distribution works with no args

--- src/distribution.py
+++ src/distribution.py
@@ -14,25 +14,38 @@ def save_stats():
         json.dump(stats, file, indent=4)
 
 
-def main():
-    if len(sys.argv[1:]) != 1:
-        raise ValueError("Too many arguments")
-    path = "data/leaves/images/" + sys.argv[1] + "_*"
-    if sys.argv[1] != "Apple" and sys.argv[1] != "Grape":
-        raise ValueError("No such plant type")
+def create_graph(plant):
+    path = f"data/leaves/images/{plant}_*"
     data = []
     for dir in glob.glob(path):
         count = len(os.listdir(dir))
         data.append((os.path.basename(dir), count))
-    fig, ax = plt.subplots(1, 2, figsize=(15,8))
+    fig, ax = plt.subplots(1, 2, figsize=(15, 8))
     colors = ['#636EFA', '#EF553B', '#00CC96', '#AB63FA']
-    ax[0].set_title(sys.argv[1] + " class distribution")
-    ax[0].pie([j for (i,j) in data], colors=colors, autopct='%1.1f%%')
-    ax[1].bar([i for (i,j) in data], [j for (i,j) in data], color=colors)
+    ax[0].set_title(f"{plant} class distribution")
+    ax[0].pie([j for (i, j) in data], colors=colors, autopct='%1.1f%%')
+    ax[1].bar([i for (i, j) in data], [j for (i, j) in data], color=colors)
     ax[1].grid(axis='y')
     ax[1].set_axisbelow(True)
     ax[1].set_facecolor("#E5ECF6")
-    plt.savefig("graphs/" + sys.argv[1] + "_distribution.png")
+    plt.savefig(f"graphs/{plant}_distribution.png")
+    plt.close()
+
+
+def main():
+    args = sys.argv[1:]
+    if len(args) > 1:
+        print("Too many arguments")
+        return
+    if len(args) == 0:
+        plants = ["Apple", "Grape"]
+    else:
+        if args[0] not in ["Apple", "Grape"]:
+            print("No such plant type")
+            return
+        plants = [args[0]]
+    for plant in plants:
+        create_graph(plant)
     save_stats()
 
 
